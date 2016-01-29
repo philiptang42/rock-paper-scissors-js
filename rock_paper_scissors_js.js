6b4b0654df63150ef69ba8bf5b3ddc625dab9a2b
philiptang42 <philiptang42@gmail.com>
2016-01-29 16:27:18 -0500

    embedded if statement

--- rock_paper_scissors_js.js
+++ rock_paper_scissors_js.js
@@ -12,3 +12,15 @@ computerChoice = "PAPER";
 else {
 computerChoice = "SCISSORS";
 };
+console.log("Computer: " + computerChoice);
+var compare = function(choice1, choice2) {
+if(choice1 === choice2) {
+    return "The result is a tie!";
+} else if (choice1 === "rock") {
+    if (choice2 === "scissors")
+        return "rock wins";
+    }
+    else {
+        return "paper wins";
+    }
+};
