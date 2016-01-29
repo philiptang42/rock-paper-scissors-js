a80e3233feeb9d0508d32041448fb90c1dc18cd9
philiptang42 <philiptang42@gmail.com>
2016-01-29 13:03:35 -0500

    translating math.random into computer choice

--- rock_paper_scissors_js.js
+++ rock_paper_scissors_js.js
@@ -3,3 +3,12 @@ userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice;
 computerChoice = Math.random();
 console.log (computerChoice)
+if (computerChoice <= 0.33){
+computerChoice = "ROCK";
+}
+else if (computerChoice <= 0.66){
+computerChoice = "PAPER";
+}
+else {
+computerChoice = "SCISSORS";
+};
